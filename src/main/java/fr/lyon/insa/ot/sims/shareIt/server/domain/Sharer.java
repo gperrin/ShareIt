16d81b3b80806a6ae6dbcb37c7dfe78704be66b7
gperrin <gaetan.perrin@insa-lyon.fr>
2015-02-18 09:17:38 +0100

    lombok is out !

--- src/main/java/fr/lyon/insa/ot/sims/shareIt/server/domain/Sharer.java
+++ src/main/java/fr/lyon/insa/ot/sims/shareIt/server/domain/Sharer.java
@@ -4,23 +4,46 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Data
 @Entity
 public class Sharer {
 	@Id
 	@GeneratedValue
 	private int id;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public float getRating() {
+		return rating;
+	}
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
 	private String lastname;
 	private String firstname;
 	private int age;
 	private float rating;
-	
-	
 
 }
