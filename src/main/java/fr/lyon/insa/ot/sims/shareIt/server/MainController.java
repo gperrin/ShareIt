ae69b4a8492d6ae181fd290897e2d07b8104d212
mathis <mathis.paul@insa-lyon.fr>
2015-02-18 09:32:28 +0100

    corrigé pour pouvoir fonctionner sur la vm

--- src/main/java/fr/lyon/insa/ot/sims/shareIt/server/MainController.java
+++ src/main/java/fr/lyon/insa/ot/sims/shareIt/server/MainController.java
@@ -1,9 +1,11 @@
 package fr.lyon.insa.ot.sims.shareIt.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import fr.lyon.insa.ot.sims.shareIt.server.dao.SharerRepository;
 import fr.lyon.insa.ot.sims.shareIt.server.domain.Sharer;
@@ -22,6 +24,15 @@ public class MainController {
 		user.setLastname("michale");
 		user.setRating(2);
 		this.userRepository.save(user);
+		System.out.println("/greeting");
         return "Hello World";
     }
+	@RequestMapping
+	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	public @ResponseBody String test(){
+		System.out.println("tst");
+		return("it's working");
+	}
+	
+	
 }
