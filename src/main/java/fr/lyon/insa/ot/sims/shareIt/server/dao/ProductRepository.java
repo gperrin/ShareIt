f978786f886c49950470d2bfe99089a0fb30d53a
gperrin <gaetan.perrin@insa-lyon.fr>
2015-03-16 11:03:46 +0100

    improved search

--- src/main/java/fr/lyon/insa/ot/sims/shareIt/server/dao/ProductRepository.java
+++ src/main/java/fr/lyon/insa/ot/sims/shareIt/server/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package fr.lyon.insa.ot.sims.shareIt.server.dao;
 
 import java.util.Collection;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.lyon.insa.ot.sims.shareIt.server.domain.Product;
@@ -12,6 +14,9 @@ import fr.lyon.insa.ot.sims.shareIt.server.domain.Sharer;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 	public Collection<Product> findBySharer(Sharer sharer);
-	public Collection<Product> findByCategory(ProductCategory category);
+	public Collection<Product> findByCategory(ProductCategory category);	
+	@Query("SELECT p FROM Product p WHERE difference(p.name,:name) > 2")
+	public Collection<Product> findByNameLike(@Param("name") String name);   
+
 
 }
