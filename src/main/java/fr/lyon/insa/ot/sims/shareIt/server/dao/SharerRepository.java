556112b9adf1767aa7d64abb0f8f7135dff8bcf6
mathis <mathis.paul@insa-lyon.fr>
2015-03-23 09:38:27 +0100

    modifié requete geospatiale

--- src/main/java/fr/lyon/insa/ot/sims/shareIt/server/dao/SharerRepository.java
+++ src/main/java/fr/lyon/insa/ot/sims/shareIt/server/dao/SharerRepository.java
@@ -2,6 +2,7 @@ package fr.lyon.insa.ot.sims.shareIt.server.dao;
 
 import java.util.Collection;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import fr.lyon.insa.ot.sims.shareIt.server.domain.Sharer;
 public interface SharerRepository extends CrudRepository<Sharer, Integer>{
 	
 	public Collection<Sharer> findByPostCode(int postCode);
-
+	@Query(value = "select distinct * from sharer s inner join sharer t on ST_DWithin(s.location, t.location,?2) and s.id != t.id and s.id=?1 order by st_distance(s.location, t.location)", nativeQuery=true)
+	public Collection<Sharer> findNearbySharers(int sharerId, int distance);
 }
