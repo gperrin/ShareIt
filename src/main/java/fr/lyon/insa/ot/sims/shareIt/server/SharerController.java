7260be75fa06017a735c543403bd2cf577d00c5b
mathis <mathis.paul@insa-lyon.fr>
2015-03-23 16:06:56 +0100

    ajout de 2 api pour upload / download des photos de profil utilisateur

--- src/main/java/fr/lyon/insa/ot/sims/shareIt/server/SharerController.java
+++ src/main/java/fr/lyon/insa/ot/sims/shareIt/server/SharerController.java
@@ -1,19 +1,21 @@
 package fr.lyon.insa.ot.sims.shareIt.server;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 import fr.lyon.insa.ot.sims.shareIt.server.domain.Sharer;
 import fr.lyon.insa.ot.sims.shareIt.server.domain.events.UserCreatedEvent;
@@ -125,5 +127,28 @@ public class SharerController extends GenericController{
 	public @ResponseBody Sharer updateLocation(@PathVariable("id")int id, @RequestParam(value = "x", required=true )double x, 
 			@RequestParam(value = "y", required = true)double y){
 		return this.sharerService.updateLocation(id, x, y);
+	}
+	
+	@RequestMapping ( method = RequestMethod.POST, value = "/user/{id:[\\d]+}/picture")
+	public void uploadProfilePic(@PathVariable("id") int id, @RequestParam(value="file", required = true) MultipartFile file){
+		Sharer sharer = this.sharerService.getUser(id);
+		if ( sharer == null ) throw new ResourceNotFoundException("User", id);
+		try {
+			sharer.setProfilePicture(file.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@RequestMapping ( method = RequestMethod.GET, value = "/user/{id:[\\d]+}/picture")
+	public void downloadProfilePic ( @PathVariable ("id")int id, HttpServletResponse response){
+		Sharer sharer = this.sharerService.getUser(id);
+		try {
+			response.getOutputStream().write(sharer.getProfilePicture());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.setContentType(sharer.getProFilePictureType().toString());
+		//response.setContentType("image/jpeg");
 	}
 }
